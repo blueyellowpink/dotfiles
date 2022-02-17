ac56bb2688e8a2acb25488558a8f71ff2e22abbe
trasua <hyouka266@gmail.com>
2022-02-17 16:13:10 +0700

    Update hop.lua

--- .config/nvim/after/plugin/hop.lua
+++ .config/nvim/after/plugin/hop.lua
@@ -6,13 +6,13 @@ hop.setup()
 vim.api.nvim_set_keymap('n', 'f', "<cmd>lua require'hop'.hint_words()<cr>", {})
 vim.api.nvim_set_keymap('n', 'F', "<cmd>lua require'hop'.hint_char1()<cr>", {})
 
-vim.api.nvim_set_keymap('o', 'f', "<cmd>lua require'hop'.hint_words({ inclusive_jump = true })<cr>", {})
-vim.api.nvim_set_keymap('o', 'F', "<cmd>lua require'hop'.hint_char1({ inclusive_jump = true })<cr>", {})
+vim.api.nvim_set_keymap('o', 'f', "<cmd>lua require'hop'.hint_char1({ inclusive_jump = true })<cr>", {})
+vim.api.nvim_set_keymap('o', 'F', "<cmd>lua require'hop'.hint_words({ inclusive_jump = true })<cr>", {})
 
 vim.api.nvim_set_keymap('v', 'f', "<cmd>lua require'hop'.hint_words({ inclusive_jump = true })<cr>", {})
 
-vim.api.nvim_set_keymap('o', 't', "<cmd>lua require'hop'.hint_words()<cr>", {})
-vim.api.nvim_set_keymap('o', 'T', "<cmd>lua require'hop'.hint_char1()<cr>", {})
+vim.api.nvim_set_keymap('o', 't', "<cmd>lua require'hop'.hint_char1()<cr>", {})
+vim.api.nvim_set_keymap('o', 'T', "<cmd>lua require'hop'.hint_words()<cr>", {})
 
 vim.api.nvim_set_keymap('v', 't', "<cmd>lua require'hop'.hint_words()<cr>", {})
 
