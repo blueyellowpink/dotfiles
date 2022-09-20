efe6167050f76ebf0c41c8fe45c6c8a2df67333b
trasua <andrew@trasua.dev>
2022-09-20 16:55:29 +0700

    Update hop.lua

--- .config/nvim/after/plugin/hop.lua
+++ .config/nvim/after/plugin/hop.lua
@@ -9,17 +9,9 @@ vim.api.nvim_set_keymap('n', 'F', "<cmd>lua require'hop'.hint_char1()<cr>", {})
 vim.api.nvim_set_keymap('o', 'f', "<cmd>lua require'hop'.hint_char1({ inclusive_jump = true })<cr>", {})
 vim.api.nvim_set_keymap('o', 'F', "<cmd>lua require'hop'.hint_words({ inclusive_jump = true })<cr>", {})
 
-vim.api.nvim_set_keymap('v', 'f', "<cmd>lua require'hop'.hint_char1({ inclusive_jump = true })<cr>", {})
-vim.api.nvim_set_keymap('v', 'F', "<cmd>lua require'hop'.hint_words({ inclusive_jump = true })<cr>", {})
+vim.api.nvim_set_keymap('v', 'f', "<cmd>lua require'hop'.hint_words({ inclusive_jump = true })<cr>", {})
 
-vim.api.nvim_set_keymap('o', 't', "<cmd>lua require'hop'.hint_char1()<cr>", {})
-vim.api.nvim_set_keymap('o', 'T', "<cmd>lua require'hop'.hint_words()<cr>", {})
+vim.api.nvim_set_keymap('o', 't', "<cmd>lua require'hop'.hint_char1({ current_line_only = true })<cr>", {})
+vim.api.nvim_set_keymap('o', 'T', "<cmd>lua require'hop'.hint_words({ current_line_only = true })<cr>", {})
 
-vim.api.nvim_set_keymap('v', 't', "<cmd>lua require'hop'.hint_char1()<cr>", {})
-vim.api.nvim_set_keymap('v', 'T', "<cmd>lua require'hop'.hint_words()<cr>", {})
-
--- vim.api.nvim_set_keymap('n', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = false })<cr>", {})
--- vim.api.nvim_set_keymap('o', 'f', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = false, inclusive_jump = true })<cr>", {})
--- vim.api.nvim_set_keymap('o', 'F', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = false, inclusive_jump = true })<cr>", {})
--- vim.api.nvim_set_keymap('', 't', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.AFTER_CURSOR, current_line_only = false })<cr>", {})
--- vim.api.nvim_set_keymap('', 'T', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = false })<cr>", {})
+vim.api.nvim_set_keymap('v', 't', "<cmd>lua require'hop'.hint_words()<cr>", {})
