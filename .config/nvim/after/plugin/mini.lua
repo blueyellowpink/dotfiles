8b459e4c0e30ac32f3fb41eab05f2268763e79f0
blueyellowpink <andrew@trasua.dev>
2023-04-19 00:34:40 +0700

    update mini

--- .config/nvim/after/plugin/mini.lua
+++ .config/nvim/after/plugin/mini.lua
@@ -1,4 +1,7 @@
 require('mini.cursorword').setup()
+vim.cmd('hi! link MiniCursorword Visual')
+vim.cmd('hi! MiniCursorwordCurrent gui=nocombine guifg=NONE guibg=NONE')
+
 require('mini.surround').setup({
 	mappings = {
 		add = 'ma', -- Add surrounding in Normal and Visual modes
@@ -23,46 +26,8 @@ require('mini.move').setup({
 	}
 })
 require('mini.comment').setup()
--- require('mini.statusline').setup()
 require('mini.basics').setup()
 require('mini.indentscope').setup()
 require('mini.bufremove').setup()
 require('mini.trailspace').setup()
 require('mini.starter').setup()
--- require('mini.jump').setup()
--- require('mini.completion').setup()
--- require('mini.jump2d').setup({
--- 	view = {
--- 		-- Whether to dim lines with at least one jump spot
--- 		dim = true,
---
--- 		-- How many steps ahead to show. Set to big number to show all steps.
--- 		n_steps_ahead = 3,
--- 	},
---
--- 	-- Which lines are used for computing spots
--- 	allowed_lines = {
--- 		blank = true, -- Blank line (not sent to spotter even if `true`)
--- 		cursor_before = true, -- Lines before cursor line
--- 		cursor_at = true, -- Cursor line
--- 		cursor_after = true, -- Lines after cursor line
--- 		fold = true, -- Start of fold (not sent to spotter even if `true`)
--- 	},
---
--- 	-- Which windows from current tabpage are used for visible lines
--- 	allowed_windows = {
--- 		current = true,
--- 		not_current = true,
--- 	},
---
--- 	-- Functions to be executed at certain events
--- 	hooks = {
--- 		before_start = nil, -- Before jump start
--- 		after_jump = nil, -- After jump was actually done
--- 	},
---
--- 	-- Module mappings. Use `''` (empty string) to disable one.
--- 	mappings = {
--- 		start_jumping = 'f',
--- 	},
--- })
