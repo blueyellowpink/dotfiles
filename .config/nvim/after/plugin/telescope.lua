dba1589c412c5519a1edac4cab234730d14da6e8
blueyellowpink <andrew@trasua.dev>
2023-05-09 23:05:14 +0700

    update telescope

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -15,10 +15,28 @@ require('telescope').setup {
                 ["<C-k>"] = actions.preview_scrolling_up
             },
         },
+        prompt_prefix = "   ",
+        selection_caret = "  ",
+        sorting_strategy = "ascending",
+        layout_strategy = "horizontal",
+        layout_config = {
+            horizontal = {
+                prompt_position = "top",
+                preview_width = 0.55,
+                results_width = 0.8,
+            },
+            vertical = {
+                mirror = false,
+            },
+            width = 0.87,
+            height = 0.80,
+            preview_cutoff = 120,
+        },
     },
     pickers = {
         file_files = {
-            find_command = {'rg', '--files', '--hidden', '--glob', '!.git' }
+            find_command = {'rg', '--files', '--hidden', '--glob', '!.git' },
+            theme = 'dropdown'
         }
-    }
+    },
 }
